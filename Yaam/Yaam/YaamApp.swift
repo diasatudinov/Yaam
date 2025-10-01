596a641a07e62982cf11302ed8210305c7d85ca1
Dias <dias.atudinov@gmail.com>
2025-10-01 18:43:16 +0500

    app setup

---
+++ Yaam/Yaam/YaamApp.swift
@@ -0,0 +1,17 @@
+//
+//  YaamApp.swift
+//  Yaam
+//
+//  Created by Dias Atudinov on 01.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct YaamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
