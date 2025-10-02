b08844389b542e814ac03a9a1cc699fa32e45dd0
Dias <dias.atudinov@gmail.com>
2025-10-02 20:00:04 +0500

    add menu

--- Yaam/Yaam/YModules/YMenu/YLoaderView.swift
+++ Yaam/Yaam/YModules/YMenu/YLoaderView.swift
@@ -2,16 +2,83 @@
 //  YLoaderView.swift
 //  Yaam
 //
-//  Created by Dias Atudinov on 02.10.2025.
 //
 
 import SwiftUI
 
 struct YLoaderView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @State private var scale: CGFloat = 1.0
+        @State private var progress: CGFloat = 0.0
+        @State private var timer: Timer?
+        var body: some View {
+            ZStack {
+                ZStack {
+                    Image(.loaderViewLogoY)
+                        .resizable()
+                        .ignoresSafeArea()
+                        .scaledToFill()
+                    Color.black.opacity(0.5)
+                        .ignoresSafeArea()
+                }
+                
+                
+                VStack(spacing: 0) {
+                    
+                    Image(.loaderViewLogoY)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 200)
+                        .cornerRadius(16)
+                        .scaleEffect(scale)
+                        .animation(
+                            Animation.easeInOut(duration: 0.8)
+                                .repeatForever(autoreverses: true),
+                            value: scale
+                        )
+                        .onAppear {
+                            scale = 0.8
+                        }
+                    
+                    VStack(spacing: 12) {
+                        Text("\(Int(progress * 100))%")
+                            .font(.system(size: 28, weight: .bold, design: .rounded))
+                            .foregroundStyle(.white)
+                            .monospacedDigit()
+                        
+                        ProgressView(value: progress)
+                            .progressViewStyle(.linear)
+                            .frame(width: UIScreen.main.bounds.width/2, height: 8)
+                            .tint(.blue)
+                            .padding(5)
+                            .background(.black.opacity(0.5))
+                            .clipShape(Capsule())
+                    }
+                    .padding()
+                    .onAppear { startTimer() }
+                    .onDisappear { timer?.invalidate() }
+                }
+                
+                
+                
+            }
+            .onAppear {
+                startTimer()
+            }
+        }
+        
+        func startTimer() {
+            timer?.invalidate()
+            progress = 0
+            timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+                if progress < 1 {
+                    progress += 0.01
+                } else {
+                    timer.invalidate()
+                }
+            }
+        }
     }
-}
+
 
 #Preview {
     YLoaderView()
--- Yaam/Yaam/YModules/YMenu/YMenuView.swift
+++ Yaam/Yaam/YModules/YMenu/YMenuView.swift
@@ -2,14 +2,105 @@
 //  YMenuView.swift
 //  Yaam
 //
-//  Created by Dias Atudinov on 02.10.2025.
 //
 
 import SwiftUI
 
 struct YMenuView: View {
+    @State private var showGame = false
+    @State private var showShop = false
+    @State private var showAchievement = false
+    @State private var showMiniGames = false
+    @State private var showSettings = false
+    @State private var showCalendar = false
+    @State private var showDailyReward = false
+    
+    //    @StateObject var shopVM = CPShopViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            
+            VStack(spacing: 0) {
+                
+                HStack {
+                    
+                    Button {
+                        showSettings = true
+                    } label: {
+                        Image(.settingsIconY)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 100:85)
+                    }
+                    
+                    Spacer()
+                    
+                    ZZCoinBg()
+                    
+                    
+                }.padding(20)
+                Spacer()
+                
+                
+            }
+            
+            VStack(spacing: 0) {
+                
+                Button {
+                    showShop = true
+                } label: {
+                    Image(.shopIconY)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 140:105)
+                }
+                
+                Button {
+                    showGame = true
+                } label: {
+                    Image(.playIconY)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 140:105)
+                }
+                
+                Button {
+                    showAchievement = true
+                } label: {
+                    Image(.achievementsIconY)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 140:105)
+                }
+                
+            }
+            
+        }.frame(maxWidth: .infinity)
+            .background(
+                ZStack {
+                    Image(.menuBgY)
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .scaledToFill()
+                }
+            )
+            .fullScreenCover(isPresented: $showGame) {
+                //                LevelPickerView()
+            }
+            .fullScreenCover(isPresented: $showAchievement) {
+                YAchievementsView()
+            }
+            .fullScreenCover(isPresented: $showShop) {
+                //                ZZShopView(viewModel: shopVM)
+            }
+            .fullScreenCover(isPresented: $showSettings) {
+                YSettingsView()
+            }
+            .fullScreenCover(isPresented: $showDailyReward) {
+                //                ZZDailyView()
+            }
     }
 }
 
